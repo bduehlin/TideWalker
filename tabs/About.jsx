8f0d877c78ee02476e02b71bdcf9a48848f9a8bc
bduehlin <bduehlin@gmail.com>
2022-04-01 10:11:31 -0700

    added more detail

--- tabs/About.jsx
+++ tabs/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
 
     return (
         <View style={[styles.parent]}>
-            <Text style={[styles.text, { width: '100%', padding: 15, backgroundColor: '#ffaa85'}]}>about</Text>
+            <Text style={[styles.text, { width: '100%', padding: 15, backgroundColor: '#ffaa85'}]}>I like to take long walks on the beach</Text>
             <Text style={{
                 textAlign: 'auto',
                 fontSize: 15, 
@@ -14,7 +14,8 @@ const About = () => {
             }}>this app was created in march of 2022 by brian uehlin for the Coding Dojo bootcamp {'\n'}{'\n'}
             it utilizes the NOAA CO-OPS api to get data from US tide data stations {'\n'}
             it utilizes the google maps api to geocode user searches {'\n'}
-            the source code can be found at github.com/bduehlin</Text>
+            it is an expo-managed project utilizing many packages {'\n'}
+            the source code can be found at github.com/bduehlin/TideWalker</Text>
         </View>
     )
 }
